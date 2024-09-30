cf060d19c0843008e7cd3a31560ad19e10b5f85f
dldkddkd <jisoolee1997@naver.com>
2024-09-30 14:15:19 +0900

    docs : 머신러닝 안쓰는 파일 정리, print부분 추가

--- ml/src/model/svm.py
+++ ml/src/model/svm.py
@@ -16,20 +16,11 @@ def svc_tuning(target: str, train: pd.DataFrame, valid: pd.DataFrame, seed: int 
         train = train.sample(n=sample_size, random_state=seed)
         valid = valid.sample(n=sample_size, random_state=seed)
     # svc = SVC(random_state=seed, kernel='rbf', gamma="scale")
-    svc = SVC(kernel='rbf', random_state=seed)
-    random_search = RandomizedSearchCV(svc, param_grid, refit=True, cv=5, verbose=2, random_state=seed)
-    random_search.fit(train.drop(columns=target), train[target])
-    print("svc:", random_search.score(valid.drop(columns=target), valid[target]))  
-    result = random_search.cv_results_.get('params')[random_search.cv_results_.get('rank_test_score').argmin()]
-    gamma = result.get('gamma')
-    coef0 = result.get('coef0')
-    C = result.get('C')
-
-    ## SVM
-    # 이상치 처리에 민감하지 않음
-    # 복잡한 데이터셋 처리 가능
     for kernel in ('poly', 'rbf'):
-        svc = SVC(kernel=kernel, random_state=seed, gamma=gamma, coef0=coef0, C=C)
-        svc.fit(train.drop(columns=target), train[target])
-        result = (svc.predict(train.drop(columns=target)) == train[target]).mean()
-        print(kernel, result)
+        svc = SVC(kernel=kernel, random_state=seed)
+        random_search = RandomizedSearchCV(svc, param_grid, refit=True, cv=5, verbose=0, random_state=seed)
+        random_search.fit(train.drop(columns=target), train[target])
+        print(f"svc kernel({kernel}) best score:", random_search.score(valid.drop(columns=target), valid[target]))  
+        result = random_search.cv_results_.get('params')[random_search.cv_results_.get('rank_test_score').argmin()]
+        print(f"svc kernel({kernel}) best hyperparameter:", result)
+        print()
